c3dd9cc35b5174007ba128ca815b0ad6bd497ee4
Clayton Collins, Dylan Stackhouse, Philip Putnam and Sean Peterson <clayton@>
2017-03-06 15:31:02 -0800

    add random growth and steps

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,5 +1,26 @@
 function Game() {
     this.board = new Board(100,100)
+    this.log = [];
+    this.playerArray = [new Player(0,"string"), new Player(1,"potato")];
+    this.activePlayer = 0;
+    this.run = false;
+    this.historyArray = [[],[]];
+    this.winner = 0;
+    this.steps = 0;
+
+
+
+}
+Game.prototype.endGame = function() {
+    if((this.board.x * this.board.y) === (this.playerArray[0].score + this.playerArray[1].score)){
+        if(this.playerArray[0].score > this.playerArray[1].score) {
+            this.winner = 0;
+        } else {
+            this.winner = 1;
+        }
+        $("#end-game").text("GAME OVER Player 1 score: "+this.playerArray[0].score+" player2score: "+this.playerArray[1].score);
+
+    }
 }
 
 Game.prototype.playerClick = function(canvas) {
@@ -8,13 +29,23 @@ Game.prototype.playerClick = function(canvas) {
         var bb = canvas.getBoundingClientRect();
         var x = e.clientX - bb.left;
         var y = e.clientY - bb.top;
-        console.log(x+", "+y);
         x = Math.floor(x/(500/game.board.x));
         y = Math.floor(y/(500/game.board.y));
         game.board.grid[x][y].active = true;
+        game.board.grid[x][y].player = game.activePlayer;
+        game.playerArray[game.board.grid[x][y].player].score ++;
+        game.historyArray[game.board.grid[x][y].player].push([x,y]);
     });
-}
+};
 
+
+function Player(id,style) {
+    this.id = id,
+    this.style = "linear",
+    this.score = 0,
+    this.active = false
+    // this.history =
+}
 
 function Board(x,y) {
     this.x = x,
@@ -23,7 +54,7 @@ function Board(x,y) {
 }
 
 
-Board.prototype.grow = function() {
+Board.prototype.grow = function(game) {
     var coords = [];
     for (var i=0;i<this.x;i++) {
         for (var j=0;j<this.y;j++) {
@@ -31,32 +62,50 @@ Board.prototype.grow = function() {
                 this.grid[i][j].age ++;
                 if(i>0){
                     if(!(this.grid[i-1][j].active)){
-                        coords.push([i-1,j]);
+                        if(Math.random()>.7){
+                            coords.push([i-1,j]);
+                            this.grid[i-1][j].player = this.grid[i][j].player;
+                        }
                     }
                 }
                 if(i<this.x-1){
                     if(!(this.grid[i+1][j].active)){
-                        coords.push([i+1,j]);
+                        if(Math.random()>.7){
+                            coords.push([i+1,j]);
+                            this.grid[i+1][j].player = this.grid[i][j].player;
+                        }
                     }
                 }
                 if(j>0){
                     if(!(this.grid[i][j-1].active)){
-                        coords.push([i,j-1]);
+                        if(Math.random()>.7){
+                            coords.push([i,j-1]);
+                            this.grid[i][j-1].player = this.grid[i][j].player;
+                        }
                     }
                 }
                 if(j<this.y-1){
                     if(!(this.grid[i][j+1].active)){
-                        coords.push([i,j+1]);
+                        if(Math.random()>.7){
+
+                            coords.push([i,j+1]);
+                            this.grid[i][j+1].player = this.grid[i][j].player;
+                        }
                     }
                 }
             }
         }
     }
-    this.spread(coords);
+    this.spread(coords, game);
 }
-Board.prototype.spread = function(coords) {
+Board.prototype.spread = function(coords, game) {
     for (var i=0;i<coords.length;i++) {
-        this.grid[coords[i][0]][coords[i][1]].active = true;
+        if(!(this.grid[coords[i][0]][coords[i][1]].active)){
+            this.grid[coords[i][0]][coords[i][1]].active = true;
+            game.playerArray[this.grid[coords[i][0]][coords[i][1]].player].score ++;
+
+        }
+
     }
 }
 
@@ -95,12 +144,17 @@ function Tile(xWidth,yWidth,xPos,yPos) {
     this.width=(500/yWidth),
     this.active = false;
     this.age = 0;
+    this.player = 0;
 }
 
 Tile.prototype.draw = function(ctx) {
     ctx.beginPath();
     ctx.rect(this.xPos,this.yPos,this.width,this.height);
-    ctx.fillStyle = "rgba(0,60,0,"+this.age/100+")";
+    if(this.player === 0){
+        ctx.fillStyle = "rgba(0,60,0,1)";
+    } else {
+        ctx.fillStyle = "rgba(30,0,30,1)";
+    }
     if(this.active) {
         ctx.fill();
     }
@@ -114,8 +168,7 @@ Tile.prototype.draw = function(ctx) {
 $(document).ready(function(){
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
-
-
+    var steps = 110;
     var game = new Game;
     game.board.fill();
 
@@ -123,8 +176,32 @@ $(document).ready(function(){
 
     function draw(){
         ctx.clearRect(0,0,canvas.width,canvas.height);
-        game.board.grow();
+        if (game.run){
+            game.board.grow(game);
+            steps += 1;
+        }
+        console.log(steps);
+        if (steps > 20) {
+            game.run = false;
+            steps = 0;
+            console.log(game.historyArray);
+        }
         game.board.draw(ctx);
+        game.endGame();
+        // debugger;
     }
-    drawInterval = setInterval(draw, 10);
+
+    $('#player1').click(function(){
+        game.activePlayer = 0;
+    })
+
+    $('#player2').click(function(){
+        game.activePlayer = 1;
+    })
+
+    drawInterval = setInterval(draw, 100);
+
+    $('#start').click(function(){
+        game.run = true;
+    })
 })
